5d9c565d1bcc57dcec6cec55655c9475b15922e9
krasi070 <krasi0707@gmail.com>
2019-12-10 12:50:19 +0000

    added tile hover effect

--- Match-Three/Assets/Scripts/Board.cs
+++ Match-Three/Assets/Scripts/Board.cs
@@ -5,6 +5,8 @@ public class Board : MonoBehaviour
     public int rows;
     public int columns;
 
+    private Tile[,] _tiles;
+
     private TileType[] _types = new TileType[]
     {
         TileType.Apple,
@@ -25,6 +27,7 @@ public class Board : MonoBehaviour
         float height = 1f;
         float width = 1f;
 
+        _tiles = new Tile[rows, columns];
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < columns; col++)
@@ -32,11 +35,17 @@ public class Board : MonoBehaviour
                 TileType randomType = _types[Random.Range(0, _types.Length)];
                 GameObject tile = new GameObject(randomType.ToString().ToLower());
 
+                _tiles[row, col] = tile.AddComponent<Tile>();
+                _tiles[row, col].Coordinates = new Vector2Int(col, row);
+
                 SpriteRenderer renderer = tile.AddComponent<SpriteRenderer>();
                 renderer.sprite = Resources.Load<Sprite>($"Sprites/{randomType.ToString().ToLower()}");
 
                 height = renderer.bounds.size.y;
                 width = renderer.bounds.size.x;
+
+                BoxCollider2D collider = tile.AddComponent<BoxCollider2D>();
+                collider.size = new Vector2(width - width * 0.05f, height - height * 0.05f);
 
                 float startX = -width * (rows - 1) / 2;
                 float startY = -height * (columns - 1) / 2;
---
+++ Match-Three/Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector2Int Coordinates { get; set; }
+
+    private const string hoverEffectName = "HoverEffect";
+
+    private void OnMouseOver()
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject hoverEffect = new GameObject(hoverEffectName);
+            SpriteRenderer renderer = hoverEffect.AddComponent<SpriteRenderer>();
+            renderer.sprite = Resources.Load<Sprite>("Sprites/select");
+            renderer.color = new Color(1, 1, 1, 0.5f);
+
+            hoverEffect.transform.position = transform.position + Vector3.forward;
+            hoverEffect.transform.parent = transform;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (transform.GetChild(0).name == hoverEffectName)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
+}
