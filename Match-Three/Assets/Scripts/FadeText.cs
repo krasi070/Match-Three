05a64801fca6c51f9f6212e5d86de3e0aafd41a4
krasi070 <krasi0707@gmail.com>
2019-12-17 21:40:18 +0000

    added instructions and exit game

---
+++ Match-Three/Assets/Scripts/FadeText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FadeText : MonoBehaviour
+{
+    public float fadeSpeed;
+
+    private Text _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Fade());
+    }
+
+    private IEnumerator Fade()
+    {
+        while (true)
+        {
+            float alpha = Mathf.Abs(Mathf.Sin(Time.time * fadeSpeed));
+            _text.color = new Color(_text.color.r, _text.color.g, _text.color.b, alpha);
+
+            yield return null;
+        }
+    }
+}
---
+++ Match-Three/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    public float timeToExit;
+
+    private Image _fadeOutImage;
+
+    private void Start()
+    {
+        _fadeOutImage = GetComponentInChildren<Image>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(Exiting());
+        }
+    }
+
+    private IEnumerator Exiting()
+    {
+        float timer = 0f;
+
+        while (Input.GetKey(KeyCode.Escape))
+        {
+            timer += Time.deltaTime;
+            float alpha = Mathf.Lerp(0f, 1f, timer / timeToExit);
+            _fadeOutImage.color = new Color(_fadeOutImage.color.r, _fadeOutImage.color.g, _fadeOutImage.color.b, alpha);
+
+            if (timer >= timeToExit)
+            {
+                Application.Quit();
+            }
+
+            yield return null;
+        }
+
+        _fadeOutImage.color = new Color(_fadeOutImage.color.r, _fadeOutImage.color.g, _fadeOutImage.color.b, 0f);
+    }
+}
